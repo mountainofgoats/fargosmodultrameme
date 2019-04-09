2f693310488d633198c0ada89aaa16dc230963b5
noooooooobasssssss <49371512+noooooooobasssssss@users.noreply.github.com>
2019-04-09 19:31:27 -0400

    masomeme EX

---
+++ felinesPlayer.cs
@@ -0,0 +1,21 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM
+{
+    public class felinesPlayer : ModPlayer
+    {
+        Mod FargowiltasSouls = ModLoader.GetMod("FargowiltasSouls");
+
+        public override void PreUpdate()
+        {
+            if (felinesWorld.masoEX)
+            { 
+                if (!Main.hardMode && Main.raining && !(player.ZoneRockLayerHeight || player.ZoneDirtLayerHeight || player.ZoneUnderworldHeight))
+                {
+                    player.AddBuff(FargowiltasSouls.BuffType("Lethargic"), 2);
+                }
+            }
+        }
+    }
+}
