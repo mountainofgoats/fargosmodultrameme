ba40c753ae99547e3a6ff25ca65b204748394137
noooooooobasssssss <49371512+noooooooobasssssss@users.noreply.github.com>
2019-04-09 19:32:17 -0400

    ech

--- felinesWorld.cs
+++ felinesWorld.cs
@@ -5,5 +5,18 @@ namespace NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM
     public class felinesWorld : ModWorld
     {
         public static bool masoEX;
+        
+        public override void Load(TagCompound tag)
+        {
+            masoEX = tag.GetBool("masoEX");
+        }
+
+        public override TagCompound Save()
+        {
+            return new TagCompound
+            {
+                {"masoEX", masoEX }
+            };
+        }
     }
 }
