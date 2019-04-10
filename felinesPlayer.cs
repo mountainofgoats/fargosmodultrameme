a826b34c82c953a604d0b71b1071369f8226d3ad
noooooooobasssssss <49371512+noooooooobasssssss@users.noreply.github.com>
2019-04-10 16:35:41 -0400

    Update felinesPlayer.cs

--- felinesPlayer.cs
+++ felinesPlayer.cs
@@ -5,7 +5,18 @@ namespace NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM
 {
     public class felinesPlayer : ModPlayer
     {
+        public bool HighTemperature;
         Mod FargowiltasSouls = ModLoader.GetMod("FargowiltasSouls");
+        
+        public override void ResetEffects()
+        {
+            HighTemperature = false;
+        }
+
+        public override void UpdateDead()
+        {
+            HighTemperature = false;
+        }
 
         public override void PreUpdate()
         {
@@ -17,6 +28,33 @@ namespace NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM
                 if (player.ZoneUnderworldHeight)
                     player.AddBuff(mod.BuffType("HighTemperature"), 2);
             }
+        }
+        
+                public override void SetupStartInventory(IList<Item> items, bool mediumcoreDeath)
+        {
+            Item item = new Item();
+            item.SetDefaults(mod.ItemType("MasochistEX"));
+            items.Add(item);
+        }
+
+        public override void UpdateBadLifeRegen()
+        {
+            if (HighTemperature)
+            {
+                if (player.lifeRegen > 0)
+                {
+                    player.lifeRegen = 0;
+                }
+                player.lifeRegenTime = 0;
+                player.lifeRegen -= 30;
+            }
+        }
+
+        public override bool PreKill(double damage, int hitDirection, bool pvp, ref bool playSound, ref bool genGore, ref PlayerDeathReason damageSource)
+        {
+            if (HighTemperature && damage == 10.0 && hitDirection == 0 && damageSource.SourceOtherIndex == 8)
+                damageSource = PlayerDeathReason.ByCustomReason(player.name + " could not breathe the warm air");
+            return true;
         }
     }
 }
