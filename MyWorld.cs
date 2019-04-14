b44ccbb2ad3bafc1672001cccd43df855d2ed794
noooooooobasssssss <49371512+noooooooobasssssss@users.noreply.github.com>
2019-04-14 10:08:58 -0400

    Update and rename FelinesWorld.cs to MyWorld.cs

--- MyWorld.cs
+++ MyWorld.cs
@@ -3,7 +3,7 @@ using Terraria.ModLoader;
 
 namespace NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM
 {
-    public class FelinesWorld : ModWorld
+    public class MyWorld : ModWorld
     {
         public static bool masoEX;
         
