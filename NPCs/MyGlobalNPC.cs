3d1bda06bcbe86893029f3f2f929f8d285286ce4
noooooooobasssssss <49371512+noooooooobasssssss@users.noreply.github.com>
2019-04-14 20:31:41 -0400

    Update MyGlobalNPC.cs

--- NPCs/MyGlobalNPC.cs
+++ NPCs/MyGlobalNPC.cs
@@ -6,9 +6,13 @@ namespace NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM.NPCs
 {
     public class MyGlobalNPC : GlobalNPC
     {
+        float Counter;
+        float Counter2;
         int projectile;
         int[] positions = { 100, 200, 300, -100, -200, -300 };
         public override bool InstancePerEntity => true;
+        bool alreadyHealed = false;
+        bool EoCPhase2 = false;
 
         public override void SetDefaults(NPC npc)
         {
@@ -33,8 +37,8 @@ namespace NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM.NPCs
                 switch (npc.type)
                 {
                     case NPCID.KingSlime:
-                        npc.ai[1] += 1f;
-                        if (npc.ai[1] >= 120f)
+                        Counter += 1f;
+                        if (Counter >= 120f)
                         {
                             if (Main.rand.Next(2) == 0)
                                 NPC.NewNPC((int)npc.Center.X, (int)npc.Center.Y, NPCID.SlimeSpiked);
@@ -47,7 +51,35 @@ namespace NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM.NPCs
                                 projectile = Projectile.NewProjectile(player.Center.X + positions[i], player.Center.Y - 500, 0f, 10f, ProjectileID.SpikedSlimeSpike, 25, 0f, player.whoAmI);
                                 Main.projectile[projectile].tileCollide = false;
                             }
-                            npc.ai[1] = 0f;
+                            Counter = 0f;
+                        }
+                        break;
+                    case NPCID.EyeofCthulhu:
+                        if (npc.life <= npc.lifeMax * 0.65f && !alreadyHealed)
+                        {
+                            npc.dontTakeDamage = true;
+                            Counter += 1f;
+                            if (Counter >= 120f)
+                            {
+                                int heal = npc.lifeMax - npc.life;
+                                npc.life += heal;
+                                npc.HealEffect(heal);
+                                Counter = 0f;
+                                alreadyHealed = true;
+                                npc.dontTakeDamage = false;
+                                EoCPhase2 = true;
+                            }
+                        }
+                        if (Counter2 >= 300f && EoCPhase2)
+                        {
+                            NPC.NewNPC((int)npc.Center.X, (int)npc.Center.Y, NPCID.ServantofCthulhu);
+                            Counter2 = 0f;
+                        }
+                        if (EoCPhase2)
+                        {
+                            player.AddBuff(mod.BuffType("CthulhusAura"), 2);
+                            npc.damage *= 3;
+                            Counter2 += 1f;
                         }
                         break;
                 }
@@ -63,6 +95,9 @@ namespace NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM.NPCs
                     case NPCID.KingSlime:
                         target.AddBuff(BuffID.OgreSpit, Main.rand.Next(60, 120));
                         break;
+                    case NPCID.EyeofCthulhu:
+                        target.AddBuff(BuffID.Obstructed, Main.rand.Next(60, 120));
+                        break;
                 }
             }
         }
@@ -75,6 +110,9 @@ namespace NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM.NPCs
                 {
                     case NPCID.KingSlime:
                         Item.NewItem(npc.getRect(), mod.ItemType("SlimyShield"));
+                        break;
+                    case NPCID.EyeofCthulhu:
+                        Item.NewItem(npc.getRect(), mod.ItemType("CoreofEvil"));
                         break;
                 }
             }
