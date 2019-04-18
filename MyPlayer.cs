02e47d97ef731eb466c73b680c5cdd155f09679f
noooooooobasssssss <49371512+noooooooobasssssss@users.noreply.github.com>
2019-04-18 17:16:57 -0400

    Update MyPlayer.cs

--- MyPlayer.cs
+++ MyPlayer.cs
@@ -5,10 +5,24 @@ namespace NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM
     public class MyPlayer : ModPlayer
     {
         public bool CthulhusAura;
+        public bool RottenFlesh;
 
         public override void ResetEffects()
         {
             CthulhusAura = false;
+            RottenFlesh = false;
+        }
+        
+        public override void ModifyHitNPC(Item item, NPC target, ref int damage, ref float knockback, ref bool crit)
+        {
+            if (RottenFlesh)
+                target.AddBuff(BuffID.CursedInferno, Main.rand.Next(180, 240));
+        }
+
+        public override void ModifyHitNPCWithProj(Projectile proj, NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
+        {
+            if (RottenFlesh)
+                target.AddBuff(BuffID.CursedInferno, Main.rand.Next(180, 240));
         }
     }
 }
