2fdbf799591d2fbe7acfd06dbabfadc201577c02
noooooooobasssssss <49371512+noooooooobasssssss@users.noreply.github.com>
2019-04-19 15:52:25 -0400

    Create PlaceholderPet.cs

---
+++ Buffs/PlaceholderPet.cs
@@ -0,0 +1,31 @@
+using Terraria;
+using Terraria.ModLoader;
+
+namespace NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM.Buffs
+{
+    public class PlaceholderPet : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Placeholder Pet");
+            Description.SetDefault("Yea, its me");
+            Main.buffNoTimeDisplay[Type] = true;
+            Main.vanityPet[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            MyPlayer modPlayer = player.GetModPlayer<MyPlayer>(mod);
+            modPlayer.PlaceholderPet = true;
+            player.buffTime[buffIndex] = 18000;            
+            if (player.ownedProjectileCounts[mod.ProjectileType("PlaceholderPet")] < 1 && player.whoAmI == Main.myPlayer)
+                Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, mod.ProjectileType("PlaceholderPet"), 0, 0f, player.whoAmI, 0f, 0f);
+        }
+
+        public override bool Autoload(ref string name, ref string texture)
+        {
+            texture = "NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM/Buffs/PlaceholderBuff";
+            return true;
+        }
+    }
+}
