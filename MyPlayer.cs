fea5593f4537e8f9de770add8d814fc0ce65f7b6
noooooooobasssssss <49371512+noooooooobasssssss@users.noreply.github.com>
2019-04-19 15:57:41 -0400

    Update MyPlayer.cs

--- MyPlayer.cs
+++ MyPlayer.cs
@@ -6,11 +6,13 @@ namespace NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM
     {
         public bool CthulhusAura;
         public bool RottenFlesh;
+        public bool PlaceholderPet;
 
         public override void ResetEffects()
         {
             CthulhusAura = false;
             RottenFlesh = false;
+            PlaceholderPet = false;
         }
         
         public override void ModifyHitNPC(Item item, NPC target, ref int damage, ref float knockback, ref bool crit)
@@ -23,6 +25,13 @@ namespace NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM
         {
             if (RottenFlesh)
                 target.AddBuff(BuffID.CursedInferno, Main.rand.Next(180, 240));
+        }
+
+        public override void SetupStartInventory(IList<Item> items, bool mediumcoreDeath)
+        {
+            Item item = new Item();
+            item.SetDefaults(mod.ItemType("Placeholder"));
+            items.Add(item);
         }
     }
 }
