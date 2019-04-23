b09a60134b8ffca39c57f47a72d3a481cec5b0c6
noooooooobasssssss <49371512+noooooooobasssssss@users.noreply.github.com>
2019-04-23 17:11:13 -0400

    Beenade Launcher and Fargo Yoyo

---
+++ Items/Weapons/BeenadeLauncher.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM.Items.Weapons
+{
+    public class BeenadeLauncher : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Beenade Launcher");
+            Tooltip.SetDefault("™");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 50;
+            item.height = 50;
+            item.damage = 15;
+            item.ranged = true;
+            item.useTime = 30;
+            item.useAnimation = 30;
+            item.useStyle = 5;
+            item.UseSound = SoundID.Item11;
+            item.rare = 3;
+            item.knockBack = 6f;
+            item.shoot = ProjectileID.Beenade;
+            item.shootSpeed = 10f;
+            item.autoReuse = true;
+            item.noMelee = true;
+        }
+
+        public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(-10, 0);
+		}
+    }
+}
+ 
---
+++ Items/Weapons/FARGOYOYO.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM.Items.Weapons
+{
+    public class FARGOYOYO : ModItem
+    {
+        Mod FargowiltasSouls = ModLoader.GetMod("FargowiltasSouls");
+        public override string Texture => "NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM/Items/PlaceholderItem";
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Fargo");
+            Tooltip.SetDefault("FARGO YOYO");
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> list)
+        {
+            foreach (TooltipLine tooltipLine in list)
+            {
+                if (tooltipLine.mod == "Terraria" && tooltipLine.Name == "ItemName")
+                {
+                    tooltipLine.overrideColor = new Color?(new Color(271, 72, 62));
+                }
+            }
+        }
+
+        public override void SetDefaults()
+        {
+            item.useStyle = 5;
+            item.width = 50;
+            item.height = 50;
+            item.useAnimation = 25;
+            item.useTime = 25;
+            item.shootSpeed = 16f;     
+            item.knockBack = 6f;
+            item.damage = 400;
+            item.rare = 11;
+            item.melee = true;
+            item.channel = true;
+            item.noMelee = true;
+            item.noUseGraphic = true;
+            item.UseSound = SoundID.Item1;
+            item.value = Item.sellPrice(silver: 1);
+            item.shoot = mod.ProjectileType("FARGOYOYOPROJ");
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Terrarian);
+            recipe.AddIngredient(FargowiltasSouls.ItemType("TophatSquirrel"), 10);
+            recipe.AddTile(TileID.LunarCraftingStation);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
