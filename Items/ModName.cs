d9637e3d80988eef0654eb2d243154b23ca01f1d
catsounds <41160660+mountainofgoats@users.noreply.github.com>
2019-04-28 22:30:39 +1000

    uploaded the mod name file
    :hahayes: it finally happens

---
+++ Items/ModName.cs
@@ -0,0 +1,30 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM.Items
+{
+    public class NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM : ModItem
+    {
+        public override string Texture => "NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM/Items/PlaceholderItem";
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM");
+            Tooltip.SetDefault(
+                                @"Konami's Best: New Extra Epic Ultimate Fargo's Soul Mod++ 
+                                Dancing All Night Golden 2: Electric Boogaloo
+                                Ultra: The Game: The Book: The Movie FES Deluxe & Knuckles & Knuckles 
+                                featuring Dante from the Devil May Cry series
+                                All Stars Battle Royale + Bowser's Minions at the Olympic Games HD Turbo 
+                                Game of the Year Edition + Rabbids – New Funky Mode!"
+                               ); 
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 28;
+            item.height = 28;
+        }
+    }
+}
