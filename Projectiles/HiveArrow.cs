3d9220add1dbc6233caf6da7bfc64a5a1cefb80d
Mak0-Z <47305558+Mak0-Z@users.noreply.github.com>
2019-04-29 18:21:14 +0300

    Added hive arrow.

---
+++ Projectiles/HiveArrow.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System;
+
+namespace NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM.Projectiles
+{
+	public class HiveArrow : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Hive Arrow");
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.CloneDefaults(ProjectileID.WoodenArrowFriendly);
+			projectile.width = 14;
+			projectile.height = 32;
+			projectile.ignoreWater = true;
+			projectile.penetrate = 1;
+			projectile.arrow = true;
+			projectile.melee = false;
+			projectile.friendly = true;
+			projectile.tileCollide = true;
+		}
+
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit) => NPC.NewNPC((int)projectile.position.X, (int)projectile.position.Y, NPCID.Bee);
+
+
+		public override void Kill(int timeLeft)
+		{
+			Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.t_Honey);
+			Collision.HitTiles(projectile.position, projectile.velocity, projectile.width, projectile.height);
+		}
+	}
+}
