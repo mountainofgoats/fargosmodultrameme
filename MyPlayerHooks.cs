219dbc1eae643af0d68d5cca2360a30f78e8e397
Mak0-Z <47305558+Mak0-Z@users.noreply.github.com>
2019-05-02 01:46:18 +0300

    better

--- MyPlayerHooks.cs
+++ MyPlayerHooks.cs
@@ -6,13 +6,13 @@ namespace NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM
 	{
 		public override void ProcessTriggers(TriggersSet triggersSet)
 		{
-			string[] stones = new string[] { "MindStoneBuff", "PowerStoneBuff", "RealityStoneBuff", "SoulStoneBuff", "SpaceStoneBuff", "TimeStoneBuff" };
+			string[] stones = new string[] { "MindStone", "PowerStone", "RealityStone", "SoulStone", "SpaceStone", "TimeStone" };
 
 			if (stoneAbility.JustPressed)
 			{
 				for (int i = 0; i < stones.Length; i++)
 				{
-					if (player.HasBuff(mod.BuffType(stones[i])))
+					if (player.HasItem(mod.ItemType(stones[i])))
 					{
 						//create a new projectile depending on the type of the stone, with different colors.
 					}
