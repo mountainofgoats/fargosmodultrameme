cbd9eb7d032c68a225b53f541ff8d1973b4eadac
noooooooobasssssss <49371512+noooooooobasssssss@users.noreply.github.com>
2019-05-02 21:53:23 -0400

    the begining of firework class
    currently added the core files for damage type and first weapon with firework damage

---
+++ Items/FireworkClass/FireworkDamagePlayer.cs
@@ -0,0 +1,36 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace FargowiltasDLC.Items.FireworkClass
+{
+    // This class stores necessary player info for our custom damage class, such as damage multipliers and additions to knockback and crit.
+    public class FireworkDamagePlayer : ModPlayer
+    {
+        public static FireworkDamagePlayer ModPlayer(Player player)
+        {
+            return player.GetModPlayer<FireworkDamagePlayer>();
+        }
+
+        // Vanilla only really has damage multipliers in code
+        // And crit and knockback is usually just added to
+        // As a modder, you could make separate variables for multipliers and simple addition bonuses
+        public float fireworkDamage = 1f;
+        public int fireworkCrit;
+
+        public override void ResetEffects()
+        {
+            ResetVariables();
+        }
+
+        public override void UpdateDead()
+        {
+            ResetVariables();
+        }
+
+        private void ResetVariables()
+        {
+            fireworkDamage = 1f;
+            fireworkCrit = 0;
+        }
+    }
+}
---
+++ Items/FireworkClass/FireworkT1.cs
@@ -0,0 +1,42 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using FargowiltasDLC.Items.FireworkClass;
+
+namespace NEEUFSMG2EBTGTBTMFESKKDDMCHDTENFM.Items.FireworkClass
+{
+    public class FireworkT1 : FireworkDamageItem
+    {
+        public override string Texture => "FargowiltasDLC/Items/PlaceholderItem";
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Firework Tier 1");
+            Tooltip.SetDefault("The most weak firework, made from wood");
+        }
+
+        public override void SafeSetDefaults()
+        {
+            item.damage = 5;
+            item.crit = 4;
+            item.width = 50;
+            item.height = 50;
+            item.useTime = 20;
+            item.useAnimation = 20;
+            item.useStyle = 5;
+            item.UseSound = SoundID.Item1;
+            item.noMelee = true;
+            item.noUseGraphic = true;
+            item.shoot = ProjectileID.RocketFireworkRed;
+            item.shootSpeed = 5f;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Wood, 20);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
