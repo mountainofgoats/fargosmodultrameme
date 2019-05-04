eea13ed273c278a968bc3206f7ac6fc359b2ae46
Mak0-Z <47305558+Mak0-Z@users.noreply.github.com>
2019-05-04 18:20:22 +0300

    u bro

--- Items/Special/InfinityGauntlet.cs
+++ Items/Special/InfinityGauntlet.cs
@@ -3,15 +3,18 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace FargowiltasDLC.Items.Special
 {
 	public class InfinityGauntlet : ModItem
 	{
+		public override string Texture => "FargowiltasDLC/Items/PlaceholderItem"; 
+
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Infinity Gauntlet");
-			Tooltip.SetDefault("A gauntlet capable of wielding the power of the 6 infinity stones.\n~~Right click to put the Infinity Stone in."); 
+			Tooltip.SetDefault("A gauntlet capable of wielding the power of the 6 infinity stones.\n~~Right click to put the Infinity Stone in.~~"); 
             Item.staff[item.type] = true;
 		}
 
@@ -34,9 +37,26 @@ namespace FargowiltasDLC.Items.Special
 			item.rare = 11;
 			item.maxStack = 1;
 			item.useStyle = 5;
-            item.useTime = 20;
-            item.useAnimation = 20;
-            item.UseSound = SoundID.Item123;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.UseSound = SoundID.Item123;
+		}
+
+		public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
+		{
+			Texture2D texture = mod.GetTexture("Items/PlaceholderItem");
+
+			spriteBatch.Draw(texture, Vector2.Zero, null, Color.White, 0, Vector2.Zero, scale, SpriteEffects.None, 0f);
+			return false;
+
+		}
+
+		public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+		{
+			Texture2D texture = mod.GetTexture("Items/Special/InfinityGauntletInventory");
+
+			spriteBatch.Draw(texture, position, null, Color.White, 0, origin, scale, SpriteEffects.None, 0f);
+			return false;
 		}
 
 		public override void AddRecipes()
