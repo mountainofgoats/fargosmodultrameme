e1497bf3fec71061481906df30351eb37a525d66
noooooooobasssssss <49371512+noooooooobasssssss@users.noreply.github.com>
2019-05-11 11:07:58 -0400

    face melter

---
+++ Projectiles/MusicalNote.cs
@@ -0,0 +1,30 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FargowiltasDLC.Projectiles
+{
+    public class MusicalNote : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Musical Note");
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 8;               
+            projectile.height = 8;              
+            projectile.aiStyle = 1;            
+            projectile.friendly = true;        
+            projectile.hostile = false;         
+            projectile.ranged = true;          
+            projectile.penetrate = -1;           
+            projectile.timeLeft = 600;          
+            projectile.ignoreWater = true;          
+            projectile.tileCollide = true;
+            projectile.scale = 1.5f;
+            projectile.light = 2f;
+            aiType = ProjectileID.Bullet;
+        }
+    }
+}
